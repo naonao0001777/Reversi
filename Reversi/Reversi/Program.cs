fbbb276fe7687e9fb229a1a8d174579eac02218e
直弘 安孫子 <abi1060special@gmail.com>
2019-08-13 03:10:11 +0900

    カーソル移動処理 石を置く処理

--- Reversi/Reversi/Program.cs
+++ Reversi/Reversi/Program.cs
@@ -8,37 +8,88 @@ namespace Reversi
 {
     class Program
     {
+        const int BOARD_WIDTH = 8;
+        const int BOARD_HEIGHT = 8;
+
+        enum Color {
+            COLOR_NONE = -1,
+            COLOR_BLACK = 0,
+            COLOR_WHITE = 1
+        };
+        
         static void Main(string[] args)
         {
             int cursorX = 0;
             int cursorY = 0;
-            int MAX_WEIGHT = 8;
-            int MAX_HEIGHT = 8;
+            
+            int[,] cells = new int[BOARD_HEIGHT, BOARD_WIDTH];
+
+            int turn = (int)Color.COLOR_BLACK;
+
+            char[][] ColorNames = new char[2][];
+            ColorNames[0] = new char[] { '黒' };
+            ColorNames[1] = new char[] { '白' };
+
+            bool CantPut = false;
+            // セル画面を初期化
+            DrawingCells();
 
             while (true)
             {
-                
-                switch (Console.Read().ToString())
-                {
-                    case "w":
-                        cursorY++;
-                        break;
-                    case "a":
-                        cursorX--;
-                        break;
-                    case "s":
-                        cursorY--;
-                        break;
-                    case "d":
-                        cursorX++;
-                        break;
-                    default:
-                        break;
-                }
+                // 画面をクリア
                 Console.Clear();
-                for (int y = 0; y < MAX_HEIGHT; y++)
+
+                // 盤面を描画
+                DrawingBoard();
+            }
+
+            // 配置可否チェック処理
+            bool CheckCanPut(int _turn, int _cursorX, int _cursorY)
+            {
+                if (cells[_cursorY,_cursorX] != (int)Color.COLOR_NONE)
                 {
-                    for (int x = 0; x < MAX_WEIGHT; x++)
+                    return false;
+                }
+                return true;
+            }
+
+            // 盤面を初期化する処理
+            void DrawingCells()
+            {
+                for (int y = 0; y < BOARD_HEIGHT; y++)
+                {
+                    for (int x = 0; x < BOARD_WIDTH; x++)
+                    {
+                        if (y == 3 && x == 3)
+                        {
+                            cells[y, x] = (int)Color.COLOR_WHITE;
+                        }
+                        else if (y == 3 && x == 4)
+                        {
+                            cells[y, x] = (int)Color.COLOR_BLACK;
+                        }
+                        else if (y == 4 && x == 4)
+                        {
+                            cells[y, x] = (int)Color.COLOR_WHITE;
+                        }
+                        else if (y == 4 && x == 3)
+                        {
+                            cells[y, x] = (int)Color.COLOR_BLACK;
+                        }
+                        else
+                        {
+                            cells[y, x] = (int)Color.COLOR_NONE;
+                        }
+                    }
+                }
+            }
+
+            // 盤面を描画する処理
+            void DrawingBoard()
+            {
+                for (int y = 0; y < BOARD_HEIGHT; y++)
+                {
+                    for (int x = 0; x < BOARD_WIDTH; x++)
                     {
                         if (cursorX == x && cursorY == y)
                         {
@@ -46,14 +97,85 @@ namespace Reversi
                         }
                         else
                         {
-                            Console.Write("・");
+                            switch (cells[y,x])
+                            {
+                                case (int)Color.COLOR_NONE:
+                                    Console.Write("・");
+                                    break;
+                                case (int)Color.COLOR_BLACK:
+                                    Console.Write("●");
+                                    break;
+                                case (int)Color.COLOR_WHITE:
+                                    Console.Write("〇");
+                                    break;
+                            }
                         }
                     }
                     Console.Write("\r\n");
                 }
+                if (CantPut)
+                {
+                    Console.Write("そこは置けません");
+                }
+                else
+                {
+                    Console.Write(ColorNames[turn]);
+                    Console.Write("のターン");
+                }
+
+                // カーソル移動
+                OperatingCursor(cursorX,cursorY,turn);
             }
-            
-            Console.ReadKey();
+
+            //　カーソル移動に関する処理
+            void OperatingCursor(int _cursorX,int _cursorY,int _turn)
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+                string cursorKey = key.Key.ToString();
+
+                CantPut = false;
+
+                switch (cursorKey)
+                {
+                    case "W":
+                        cursorY--;
+                        break;
+                    case "A":
+                        cursorX--;
+                        break;
+                    case "S":
+                        cursorY++;
+                        break;
+                    case "D":
+                        cursorX++;
+                        break;
+                    default:
+                        if (!(CheckCanPut(_turn, _cursorX, _cursorY)))
+                        {
+                            CantPut = true;
+                            break;
+                        }
+                        cells[cursorY,cursorX] = _turn;
+                        turn ^= 1;
+                        break;
+                }
+
+                // カーソルが盤外に出た場合の処理
+                if (cursorX == -1)
+                {
+                    cursorX = BOARD_WIDTH-1;
+                }else if (cursorY == -1)
+                {
+                    cursorY = BOARD_HEIGHT-1;
+                }else if (cursorX == BOARD_WIDTH)
+                {
+                    cursorX %= BOARD_WIDTH;
+                }else if (cursorY == BOARD_HEIGHT)
+                {
+                    cursorY %= BOARD_HEIGHT;
+                }
+
+            }
         }
     }
 }
